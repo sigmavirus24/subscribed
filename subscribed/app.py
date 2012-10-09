1a68feedcb878d718585802984541f37490c46e2
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-09 10:30:44 -0400

    Update robots.txt

--- subscribed/app.py
+++ subscribed/app.py
@@ -5,8 +5,11 @@ from github3 import repository, user, GitHubError
 app = Flask(__name__)
 
 ROBOTS = """User-agent: *
-Disallow: /*
+Disallow: /sigmavirus24
+Disallow: /sigmavirus24/github3.py
 """
+# Only disallow the two links on the front page until I can get requests 
+# working with the proxy
 
 
 @app.route('/')
