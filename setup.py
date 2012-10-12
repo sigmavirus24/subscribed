eb7820b2823b4e3767f92c3ebfa22344a8039aea
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-11 22:34:44 -0400

    Add a setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from re import compile
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[1] in ('submit', 'publish'):
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+sysv = sys.version[:3]
+
+packages = ['subscribed']
+requires = ['github3.py>=0.1b0', 'flask>=0.9']
+del sysv
+
+__version__ = ''
+with open('gh/__init__.py') as fd:
+    reg = compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+    for line in fd:
+        match = reg.match(line)
+        if match:
+            __version__ = match.group(1)
+            break
+
+if not __version__:
+    raise RuntimeError('Cannot find version information')
+
+
+setup(
+    name='subscribed',
+    version=__version__,
+    author='Ian Cordasco',
+    author_email='graffatcolmingov@gmail.com',
+    url='https://github.com/sigmavirus24/subscribed',
+    packages=packages,
+    include_package_data=False,
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ]
+    )
--- subscribed/__init__.py
+++ subscribed/__init__.py
@@ -1,1 +1,6 @@
 from app import app
+
+__title__ = 'subscribed'
+__author__ = 'Ian Cordasco'
+__version__ = '0.1'
+__copyright__ = 'Copyright 2012 Ian Cordasco'
