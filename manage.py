c0d3e889ad3c5e04d453bdc47fef08973d8a57b5
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-20 17:09:06 -0400

    Stream all requests now.
    Requesting /hcilab before would cause a H12 on Heroku because it wasn't
    responding quickly enough. With the streaming of the request, it should now be
    safe.

--- manage.py
+++ manage.py
@@ -4,4 +4,4 @@ from os import environ
 
 if __name__ == '__main__':
     port = int(environ.get('PORT', '5000'))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
--- subscribed/app.py
+++ subscribed/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, make_response
+from flask import (Flask, render_template, redirect, url_for, make_response,
+        Response)
 from github3 import GitHub, GitHubError
 from os import environ
 
@@ -12,17 +13,21 @@ if id and secret:
 
 app = Flask(__name__)
 
-ROBOTS = """User-agent: *
-Disallow: /sigmavirus24
-Disallow: /sigmavirus24/github3.py
-"""
-# Only disallow the two links on the front page until I can get requests
-# working with the proxy
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+# Streaming is necessary for accounts like hcilab
+# Taken directly from:
+# http://flask.pocoo.org/docs/patterns/streaming/#streaming-from-templates
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(**context)
+    rv.enable_buffering(30)
+    return rv
 
 
 @app.route('/<login>/<repo>')
@@ -32,7 +37,7 @@ def repo_subscribers(login, repo):
     except GitHubError:
         return redirect(url_for('index'))
 
-    return render_template('repo.html', repo=r)
+    return Response(stream_template('repo.html', repo=r))
 
 
 @app.route('/<login>')
@@ -46,7 +51,15 @@ def user_subscriptions(login):
     if failed or u.type.lower() != 'user':
         return redirect(url_for('index'))
 
-    return render_template('user.html', user=u)
+    return Response(stream_template('user.html', user=u))
+
+
+ROBOTS = """User-agent: *
+Disallow: /sigmavirus24
+Disallow: /sigmavirus24/github3.py
+"""
+# Only disallow the two links on the front page until I can get requests
+# working with the proxy
 
 
 @app.route('/robots.txt')
