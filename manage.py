06fdf360a45efef30ae19eeadf6ce6a1133bc5fc
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-20 16:06:24 -0400

    Set up Sentry to see if that will help.

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 from subscribed import app
 from os import environ
+from raven.contrib.flask import Sentry
 
+sentry = Sentry(app)
 
 if __name__ == '__main__':
     port = int(environ.get('PORT', '5000'))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
